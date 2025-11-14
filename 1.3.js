6ec637a3d6e806d72a933fcbe5e2e2474559ef47
rpathak13 <rudrapathak07@gmail.com>
2025-11-14 12:03:12 +1100

    part 1 question 1.3

--- 1.3.js
+++ 1.3.js
@@ -12,3 +12,14 @@ arr.push(19, 23, 30);
 arr.sort((a, b) => a - b);
 console.log("New sorted array:", arr);
 
+// Removing number 8
+let index8 = arr.indexOf(8);
+if (index8 !== -1) {
+    arr.splice(index8, 1);
+}
+// Removing number 31
+let index31 = arr.indexOf(31);
+if (index31 !== -1) {
+    arr.splice(index31, 1);
+}
+console.log("Array after removing 8 and 31:", arr);
